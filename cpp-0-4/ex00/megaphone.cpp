8a40f28b264b40219003a9651194c06b34dc6055
Slaye <uduterrage@gmail.com>
2024-05-08 18:33:51 +0200

    Add: ex00

---
+++ cpp-0-4/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 18:10:44 by slaye             #+#    #+#             */
+/*   Updated: 2024/05/08 18:33:13 by slaye            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+void	strupper(char *string)
+{
+	for (int i = 0; i < (int) std::strlen(string); i++)
+		std::cout << (char) std::toupper(string[i]);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+			strupper(argv[i]);
+		std::cout << std::endl;
+	}
+	return (0);
+}
