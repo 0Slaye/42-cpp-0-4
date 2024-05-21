f6cb8bf6dbd3aa1570d96f0f699cb0aca8c5ca09
Slaye <uduterrage@gmail.com>
2024-05-21 17:53:54 +0200

    Add: ex01

---
+++ cpp-2/ex01/includes/Fixed.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 12:59:31 by slaye             #+#    #+#             */
+/*   Updated: 2024/05/21 15:17:27 by slaye            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed {
+	public:
+		Fixed(void); // canonical
+		Fixed(Fixed const &ref); // canonical
+		~Fixed(void); // canonical
+
+		Fixed	&operator=(Fixed const &ref); // canonical
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+	private:
+		static int const	shift = 8;
+		int					value;
+};
+
+#endif
---
+++ cpp-2/ex01/sources/Fixed.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 13:02:20 by slaye             #+#    #+#             */
+/*   Updated: 2024/05/21 17:51:02 by slaye            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &ref) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = ref;
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed	&Fixed::operator=(Fixed const &ref) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->value = ref.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->value);
+}
+
+void	Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->value = raw;
+}
