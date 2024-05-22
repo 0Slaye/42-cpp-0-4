0faa84e1643e10f8b82d557af8fcee3f85852b42
Slaye <uduterrage@gmail.com>
2024-05-22 18:39:53 +0200

    Fix: working

--- cpp-2/ex01/sources/Fixed.cpp
+++ cpp-2/ex01/sources/Fixed.cpp
@@ -6,12 +6,15 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 13:02:20 by slaye             #+#    #+#             */
-/*   Updated: 2024/05/21 18:12:40 by slaye            ###   ########.fr       */
+/*   Updated: 2024/05/22 18:39:33 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cmath>
 #include "Fixed.hpp"
+
+#include <bitset>
 
 Fixed::Fixed(void) {
 	std::cout << "Default constructor called" << std::endl;
@@ -27,6 +30,11 @@ Fixed::Fixed(const int value) {
 	this->value = (value << this->shift);
 }
 
+Fixed::Fixed(const float value) {
+	std::cout << "Float constructor called" << std::endl;
+	this->value = roundf(value * (1 << this->shift));
+}
+
 Fixed::~Fixed(void) {
 	std::cout << "Destructor called" << std::endl;
 }
@@ -38,11 +46,23 @@ Fixed	&Fixed::operator=(Fixed const &ref) {
 }
 
 int	Fixed::getRawBits(void) const {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->value);
+}
+
+int	Fixed::toInt(void) const {
+	return(this->value >> this->shift);
+}
+
+float	Fixed::toFloat(void) const {
+	return(((float) this->value / (float) (1 << this->shift)));
 }
 
 void	Fixed::setRawBits(int const raw) {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->value = raw;
+}
+
+std::ostream	&operator<<(std::ostream &stream, Fixed const &fixed) {
+	stream << fixed.toFloat();
+	return (stream);
 }
--- cpp-2/ex01/sources/main.cpp
+++ cpp-2/ex01/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: slaye <slaye@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:16:15 by slaye             #+#    #+#             */
-/*   Updated: 2024/05/21 18:17:37 by slaye            ###   ########.fr       */
+/*   Updated: 2024/05/22 18:39:28 by slaye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,20 @@
 
 int main( void ) {
 	Fixed a;
-	Fixed b(a);
-	Fixed c;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
 
-	c = b;
+	a = Fixed( 1234.4321f );
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
 
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return (0);
 }
