2f1ac228fc84903702c7b8bb0d12ebe7abea8f82
0Slaye <uduterrage@gmail.com>
2024-05-31 18:26:58 +0200

    Add: starting ex00 files

---
+++ cpp-4/ex00/sources/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uwywijas <uwywijas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 14:48:24 by slaye             #+#    #+#             */
+/*   Updated: 2024/05/31 18:19:39 by uwywijas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int	main(void) {
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+
+	delete meta;
+	delete i;
+	delete j;
+	return (0);
+}
